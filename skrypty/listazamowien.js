ababc59f41f9643d57faeb364b2c1b168a608e01
Marta Piątek <martusia.piatek@gmail.com>
2018-09-03 21:42:07 +0200

    Dodanie listy oczekujących zamówień

---
+++ skrypty/listazamowien.js
@@ -0,0 +1,50 @@
+(function (window) {
+'use strict';
+var Aplikacja = window.Aplikacja || {};
+var $ = window.jQuery;
+
+function ListaZamowien(selektor){
+  if(!selektor) {
+    throw new Error('Brak selektora');
+  }
+  this.$element = $(selektor);
+  if(this.$element.length === 0){
+    throw new Error('Brak elementów odpowiadających selektorowi: ' + selektor);
+  }
+}
+
+ListaZamowien.prototype.dodajWiersz = function(zamowienie) {
+
+  var elementWiersza = new Wiersz(zamowienie);
+  this.$element.append(elementWiersza.$element);
+}
+
+function Wiersz(zamowienie){
+  var $div = $('<div></div>', {
+    'data-zamowienie': 'poleWyboru',
+    'class': 'checkbox'
+  });
+  var $etykieta = $('<label></label>');
+  var $poleWyboru = $('<input></input>', {
+    type: 'checkbox',
+    value: zamowienie.adresEmail
+  });
+  var opis = zamowienie.wielkosc + ' ';
+  if(zamowienie.smak){
+    opis += zamowienie.smak + ' ';
+  }
+  opis += zamowienie.kawa + ' ';
+  opis += ' (' + zamowienie.adresEmail + ')';
+  opis += ' [' + zamowienie.moc + 'x]';
+
+$etykieta.append($poleWyboru);
+$etykieta.append(opis);
+$div.append($etykieta);
+
+this.$element = $div;
+
+}
+
+Aplikacja.ListaZamowien = ListaZamowien;
+window.Aplikacja = Aplikacja;
+})(window);
--- skrypty/main.js
+++ skrypty/main.js
@@ -1,17 +1,23 @@
 (function(window) {
   'use strict';
   var SELEKTOR_FORMULARZA = '[data-zamowienie="formularz"]';
+  var SELEKTOR_LISTY_ZAMOWIEN = '[data-zamowienie="listaZamowien"]';
+
   var Aplikacja = window.Aplikacja;
   var Furgonetka = Aplikacja.Furgonetka;
   var BazaDanych = Aplikacja.BazaDanych;
 
   var ObslugaFormularza = Aplikacja.ObslugaFormularza;
 
+var ListaZamowien = Aplikacja.ListaZamowien;
   var mojaFurgonetka = new Furgonetka('abc-1001', new BazaDanych());
   window.mojaFurgonetka = mojaFurgonetka;
 
+var listaZamowien = new ListaZamowien(SELEKTOR_LISTY_ZAMOWIEN);
   var obslugaFormularza = new ObslugaFormularza(SELEKTOR_FORMULARZA);
-  obslugaFormularza.dodajObslugeWysylki(mojaFurgonetka.zlozZamowienie.bind(mojaFurgonetka));
-  console.log(obslugaFormularza);
+  obslugaFormularza.dodajObslugeWysylki(function(dane) {
+  mojaFurgonetka.zlozZamowienie.call(mojaFurgonetka, dane);
+  listaZamowien.dodajWiersz.call(listaZamowien, dane);
+});
 
 })(window);
